1a5af11179924db42ec7575eb8ec24f935f7f209
bhandhariya <sainiraja59@gmail.com>
2020-03-12 09:38:47 +0530

    dsfds

--- bin/db.js
+++ bin/db.js
@@ -1,7 +1,7 @@
   
 var mongoose = require('mongoose');
-var mongoURI = "mongodb://raja:raja1234@ds229186.mlab.com:29186/mentcomwebsite1";
-
+// var mongoURI = "mongodb://raja:raja1234@ds229186.mlab.com:29186/mentcomwebsite1";
+var mongoURI="mongodb+srv://raja341:raja341@cluster0-fhvpz.mongodb.net/test?retryWrites=true&w=majority"
 
 mongoose.Promise = global.Promise;
 mongoose.connect(mongoURI,{useNewUrlParser:true,useUnifiedTopology:true})
